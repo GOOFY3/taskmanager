92d3405db91d8aea09f351fecf49ae1396f6a7c7
goofy3 <aquibbaig97@gmail.com>
2018-10-07 00:07:32 +0530

    Done and Dusted

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //routes
 app.get('/', (req, res) => {
   client.lrange('tasks', 0, -1, (err, reply) => {
-      res.render('todo', {tasks:reply});
+      client.hgetall('call', (err, call) => {
+        res.render('todo', {tasks:reply, call:call});
+      });
   });
 })
 
@@ -55,6 +57,21 @@ app.post('/task/delete', (req, res) => {
           });
         }
       }
+      res.redirect('/');
+    });
+});
+
+app.post('/call/add', (req, res) => {
+    var newCall = {}
+    newCall.name = req.body.name;
+    newCall.company = req.body.company;
+    newCall.phone = req.body.phone;
+    newCall.time = req.body.time;
+    client.hmset('call', ['name', newCall.name, 'company', newCall.company, 'phone', newCall.phone, 'time', newCall.time], (err, reply) => {
+      if(err){
+        console.log(err);
+      }
+      console.log(reply);
       res.redirect('/');
     });
 });
